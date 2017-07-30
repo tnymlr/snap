369a351a2e900204bb0adbcb8eab2bef0b65a92d
Anton Ermolenko <aner@fastmail.com>
2017-07-30 16:30:05 +1000

    Fixed app to be proper GObject

--- src/utils/apps.js
+++ src/utils/apps.js
@@ -5,21 +5,29 @@ const AppInfo = Gio.AppInfo
 
 const log = require('utils/log')
 
-const App = new Lang.Class({
+const App = new GObject.Class({
     Name: 'App',
     GTypeName: 'SnapUtilsApp',
-    Extends: GObject.Object,
+    Extends: GObject.App,
+
+    Properties: {
+        'app': GObject.ParamSpec.object(
+            'app', '', '',
+            GObject.ParamFlags.READABLE | GObject.ParamFlags.WRITABLE,
+            Gio.AppInfo)
+    },
 
     _init: function(appInfo) {
-        this.appInfo = appInfo
+        this.parent({
+            'app': appInfo
+        })
     },
 
     get id() {return this.app.get_id()},
     get icon() {return this.app.get_icon()},
     get name() {return this.app.get_name()},
     get displayName() {return this.app.get_display_name()},
-    get commandline() {return this.app.get_commandline()},
-    get app() {return this.appInfo}
+    get commandline() {return this.app.get_commandline()}
 })
 
 const apps = AppInfo.get_all().map((info) => {
@@ -32,7 +40,6 @@ module.exports.all = function() {
 
 module.exports.forId = function(id) {
     const app = apps.find((app) => app.id === id)
-    log('FOUND: {}', app)
 
     return app
 }
