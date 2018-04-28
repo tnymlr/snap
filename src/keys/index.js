eb6020581003321603dc936b8bce102ee6456013
Anton Ermolenko <aner@fastmail.com>
2018-04-28 21:43:05 +1000

    Key manager had been rewritten.
    I've discovered that it causes certain troubles to follow Gnome's way
    to do things - those Lang.class and other stuff is quite obscure so
    I'm refactoring the majority of code to stay as close as possible to
    native javascript way of doing things

--- src/keys/index.js
+++ src/keys/index.js
@@ -1,82 +1,136 @@
-const log = require('utils/log')
+const log = require('utils/log');
 
-const gnome = require('gnome')
+const gnome = require('gnome');
 
-const Lang = require('lang')
-const Meta = require('gi/meta')
-const Shell = require('gi/shell')
-const Main = require('ui/main')
+const Meta = require('gi/meta');
+const Shell = require('gi/shell');
+const Main = require('ui/main');
+
+const _onActivated = (grabbers, action) => {
+    const grabber = grabbers.get(action);
+
+    if (grabber) {
+        grabber.callback(grabber.accelerator);
+    }
+};
+
+const _init = (ctx) => {
+    const grabbersMap = new Map();
+    return Object.assign({
+        grabbers: grabbersMap,
+        handler: gnome.display.connect(
+            'accelerator-activated',
+            (display, action, deviceId, timestamp) => {
+                _onActivated(grabbersMap, action);
+            })
+    }, ctx);
+};
+
+const _grab = (ctx, accelerator) => {
+    const grabbers = ctx.grabbers;
+
+    log('Trying to listen for hot key [accelerator={}]', accelerator);
+    const action = gnome.display.grab_accelerator(accelerator);
+
+    if (action === Meta.KeyBindingAction.NONE) {
+        log('Unable to grab accelerator [binding={}]', accelerator);
+
+        return (callback) => {
+            log('I was unable to grab the shortcut so this callback is inactive');
+        }
+    } else {
+        log('Grabbed accelerator [action={}]', action);
+        const name = Meta.external_binding_name_for_action(action);
+        log('Received binding name for action [name={}, action={}]',
+            name, action);
+
+        log('Requesting WM to allow binding [name={}]', name);
+        Main.wm.allowKeybinding(name, Shell.ActionMode.ALL);
+
+        return (callback) => {
+            grabbers.set(action, {
+                name: name,
+                accelerator: accelerator,
+                callback: callback
+            });
+        };
+    }
+};
+
+const _ungrab = (ctx, action) => {
+    const grabbers = ctx.grabbers;
+    const grabber = grabbers.get(action);
+
+    log('Stopping listening for [binding={}, action={}, name={}]',
+        grabber.accelerator, action, grabber.name);
+
+    const success = gnome.display.ungrab_accelerator(action);
+    if (success) {
+        log('Stopped listening for [binding={}, action={}, name={}]',
+            grabber.accelerator, action, grabber.name);
+        grabbers.delete(action);
+    } else {
+        log('Failed to stop listening for [binding={}, action={}, name={}]',
+            grabber.accelerator, action, grabber.name);
+    }
+
+    return success;
+};
+
+const _start = (ctx) => {
+    return _init(ctx);
+};
+
+const _stop = (ctx) => {
+    log('Stopping listening for all registered key bindings');
+
+    const grabbers = ctx.grabbers;
+
+    let success = true;
+    for (let [action, grabber] of grabbers.entries()) {
+        success = success && _ungrab(ctx, action)
+    }
+
+    return Object.assign({grabbers: null, handler: null}, ctx);
+};
+
+const _create = () => {
+    return {}
+};
 
 module.exports = {
-	KeyManager: new Lang.Class({
-		Name: 'SnapKeyManager',
+    create: () => {
+        let active = false;
+        let ctx = _create();
 
-		_init: function() {
-			this.grabbers = new Map()
+        return {
+            start: () => {
+                ctx = _start(ctx);
+                active = true;
+            },
 
-			this.handler = gnome.display.connect(
-				'accelerator-activated',
-				Lang.bind(this, function(display, action, deviceId, timestamp){
-					this._onAccelerator(action)
-				}))
-		},
+            stop: () => {
+                ctx = _stop(ctx);
+                active = false;
+            },
 
-		startListenFor: function(accelerator, callback){
-			log('Trying to listen for hot key [accelerator={}]', accelerator)
-			const action = gnome.display.grab_accelerator(accelerator)
+            onKey: (accelerator) => {
+                let action = null;
 
-			if(action == Meta.KeyBindingAction.NONE) {
-				log('Unable to grab accelerator [binding={}]', accelerator)
-			} else {
-				log('Grabbed accelerator [action={}]', action)
-				const name = Meta.external_binding_name_for_action(action)
-				log('Received binding name for action [name={}, action={}]',
-					name, action)
-				
-				log('Requesting WM to allow binding [name={}]', name)
-				Main.wm.allowKeybinding(name, Shell.ActionMode.ALL)
+                if(active) {
+                    const binder = _grab(ctx, accelerator);
+                    action = (callback) => {
+                        binder(callback);
+                    };
+                } else {
+                    log('Attempt to grab a key using stopped key manager. All actions are ignored');
+                    action = () => {};
+                }
 
-				this.grabbers.set(action, {
-					name: name,
-					accelerator: accelerator,
-					callback: callback
-				})
-			}
-		},
-
-		stopListenFor: function(action) {
-			const grabber = this.grabbers.get(action)
-			log('Stopping listening for [binding={}, action={}, name={}]',
-				grabber.accelerator, action, grabber.name)
-
-			const success = gnome.display.ungrab_accelerator(action)
-			if(success) {
-				log('Stopped listening for [binding={}, action={}, name={}]',
-					grabber.accelerator, action, grabber.name)
-				this.grabbers.delete(action)
-			} else {
-				log('Failed to stop listening for [binding={}, action={}, name={}]',
-					grabber.accelerator, action, grabber.name)
-			}
-
-			return success
-		},
-
-		stopListenForAll: function() {
-			log('Stopping listening for all registered key bindings')
-
-			let success = true
-			for(let [action, grabber] of this.grabbers.entries()) {
-				success = success && this.stopListenFor(action)
-			}
-		},
-
-		_onAccelerator: function(action) {
-			const grabber = this.grabbers.get(action)
-
-			if(grabber) {
-				grabber.callback(grabber.accelerator)
-			}
-		}
-	})
-}
+                return {
+                    do: action
+                };
+            }
+        }
+    }
+};
--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-const log = require('utils/log')
-const keys = require('keys')
+const log = require('utils/log');
+const keys = require('keys');
 
-const windows = require('windows')
+const windows = require('windows');
 
-const Main = require('ui/main')
-const Lang = require('lang')
+const Lang = require('lang');
 
-const settings = require('settings')
+const settings = require('settings');
 
-const manager =  new keys.KeyManager()
+const manager = keys.create();
+
 const KeyController = new Lang.Class({
 	Name: 'SnapKeyController',
 
@@ -16,51 +16,47 @@ const KeyController = new Lang.Class({
 	},
 
 	setupKeys: function(keys){
-		log('Received settings! Setting up listeners for keys...')
+		log('Received settings! Setting up listeners for keys...');
 		for(let shortcut of keys) {
-			const key = shortcut.key
-			const app = shortcut.app
+			const key = shortcut.key;
+			const app = shortcut.app;
 
-			manager.startListenFor(key, function(key) {
-				//we take all the windows
-				windows.Manager.getWindows().map((w) => {
-					const wmClass = w.wmClass.toLowerCase()
+			manager.onKey(key).do((key) => {
+                //we take all the windows
+                windows.Manager.getWindows().map((w) => {
+                    const wmClass = w.wmClass.toLowerCase();
 
-					// and we take the app and count how much words in the name
-					// of an app we can find in window class.
-					// this is 'weight' of our window
-					const window =  {
-						weight: app.toLowerCase()
-							.split(' ')
-							.filter((word) => wmClass.indexOf(word) >= 0)
-							.length,
-						window: w
-					}
-					return window
-				}).sort((a, b) => { //windows with bigger weight go to the top.
-					return a.weight < b.weight
-						? -1
-						: a.weight > b.weight
-							? 1
-							: 0
-				}).reverse().reduce((col, val) => { // and we take only windows with largest weight
-					if(col.length < 1 || col[col.length - 1].weight === val.weight) {
-						return col.concat([val])
-					}
+                    // and we take the app and count how much words in the name
+                    // of an app we can find in window class.
+                    // this is 'weight' of our window
+                    const window =  {
+                        weight: app.toLowerCase()
+                            .split(' ')
+                            .filter((word) => wmClass.indexOf(word) >= 0)
+                            .length,
+                        window: w
+                    };
+                    return window;
+                }).sort((a, b) => { //windows with bigger weight go to the top.
+                    return a.weight < b.weight
+                        ? -1
+                        : a.weight > b.weight
+                            ? 1
+                            : 0
+                }).reverse().reduce((col, val) => { // and we take only windows with largest weight
+                    if(col.length < 1 || col[col.length - 1].weight === val.weight) {
+                        return col.concat([val])
+                    }
 
-					return col
-				}, []).forEach((item) => { // bring it on
-					item.window.raise()
-				})
-			})
+                    return col
+                }, []).forEach((item) => { // bring it on
+                    item.window.raise()
+                })
+            });
 		}
 		log('Done setting up listeners for keys!')
-	},
-
-	disableKeys: function(){
-		manager.stopListenForAll()
 	}
-})
+});
 
 const SnapExtension = new Lang.Class({
 	Name: 'SnapExtension',
@@ -70,23 +66,33 @@ const SnapExtension = new Lang.Class({
 	},
 
 	enable: function(){
-		log('Enabling...')
+		log('Enabling...');
 
-		log('Loading shortcuts from settings...')
+		log('Starting key manager...');
+		manager.start();
+		log('Key manager has been started');
+
+		log('Loading shortcuts from settings...');
 		const shortcuts = settings.all.map((item) => {
-			const shortcut = {key: item.key, app: item.name}
-			log('Got shortcut [key={}, app={}]', shortcut.key, shortcut.app)
+			const shortcut = {key: item.key, app: item.name};
+			log('Got shortcut [key={}, app={}]', shortcut.key, shortcut.app);
 			return shortcut
-		})
-		log('Done loading shortcuts from settings!')
-		this.keyController.setupKeys(shortcuts)
+		});
+		log('Done loading shortcuts from settings!');
+		this.keyController.setupKeys(shortcuts);
+
+		log('Enabled!')
 	},
 
 	disable: function(){
-		log('Disabling...')
-		this.keyController.disableKeys()
-	}
-})
+		log('Disabling...');
 
-module.exports = new SnapExtension()
+		log('Stopping key manager...');
+        manager.stop();
+        log('Key manager has stopped');
+        log('Disabled!')
+	}
+});
+
+module.exports = new SnapExtension();
 
